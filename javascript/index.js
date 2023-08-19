4009de040cd9bb3ab61931b914658f7af5998c2e
Priyanshu Maurya <mauryapriyanshu-cmpn@atharvacoe.ac.in>
2023-08-19 19:49:32 +0000

    Added medium blog url
    - Added tab icon
    - structured all files

--- javascript/index.js
+++ javascript/index.js
@@ -10,13 +10,13 @@ window.onload=function(){
           body.classList.remove('dark-mode-body');
           body.classList.add('light-mode-body');
           [ ...document.getElementsByClassName("dark-mode-a") ].forEach( x => x.className = 'light-mode-a')
-          document.getElementById("divider-pic").src="divider_light.svg";
+          document.getElementById("divider-pic").src="public/divider_light.svg";
           document.getElementById("slider-circle").className="slider round slider-mode-dark";
       } else { // dark mode
           body.classList.remove('light-mode-body');
           body.classList.add('dark-mode-body');
           [ ...document.getElementsByClassName("light-mode-a") ].forEach( x => x.className = 'dark-mode-a');
-          document.getElementById("divider-pic").src="divider_dark.svg";
+          document.getElementById("divider-pic").src="public/divider_dark.svg";
           document.getElementById("slider-circle").className="slider round slider-mode-light";
       }
   });
